e3f9d0469162bcaa96dfeac55b62008345f768f0
SHIDi233 <1985136419@qq.com>
2024-04-26 16:58:43 +0800

    has too many bugs

--- src/main/java/com/studio314/autowaremanagesys/config/SpringSecurityConfig.java
+++ src/main/java/com/studio314/autowaremanagesys/config/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ import com.studio314.autowaremanagesys.component.SpringSecurityHandler;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -29,15 +30,22 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
-        http.authorizeHttpRequests(request -> {
-                    request.anyRequest().authenticated();
-                }).addFilterBefore(responseFilter, WebAsyncManagerIntegrationFilter.class) // 在 Web...过滤器之前添加过滤器
+        http    .authorizeHttpRequests(request -> {
+                    request.anyRequest().permitAll();
+//                    request.requestMatchers( "/1" , "/signup").permitAll();
+//                    request.requestMatchers("/signup/signup").permitAll();
+//                    request.anyRequest().authenticated();
+                })
+
+                .addFilterBefore(responseFilter, WebAsyncManagerIntegrationFilter.class) // 在 Web...过滤器之前添加过滤器
                 .formLogin(request -> { // 添加登录处理器
                     request.successHandler(handler)
                             .failureHandler(handler);
-                }).exceptionHandling(request -> { // 添加访问拒绝处理
-                    request.accessDeniedHandler(handler);
-                }).logout(request -> { // 添加退出处理器
+                })
+//                .exceptionHandling(request -> { // 添加访问拒绝处理
+//                    request.accessDeniedHandler(handler);
+//                })
+                .logout(request -> { // 添加退出处理器
                     request.logoutSuccessHandler(handler)
                             .addLogoutHandler(handler);
                 });
